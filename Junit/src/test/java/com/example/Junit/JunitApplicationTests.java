7685ab14f9c3f6012a7ea271b0bfc370242672e3
ashwinighorude <79485051+ashwinighorude@users.noreply.github.com>
2021-03-15 20:53:53 -0700

    Update JunitApplicationTests.java

--- Junit/src/test/java/com/example/Junit/JunitApplicationTests.java
+++ Junit/src/test/java/com/example/Junit/JunitApplicationTests.java
@@ -1,10 +1,21 @@
 
   package com.example.Junit;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.example.Junit.example.Employee;
@@ -13,6 +24,59 @@ import com.example.Junit.example.EmployeeWithHashcode;
 import com.example.Junit.example.EmployeeWithHashcodeEquals;
 
 public class JunitApplicationTests {
+	
+	
+	@Test
+	public void StreamOperations() {
+		
+		List<Integer> list= Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		assertNotNull(list);
+		 Integer[] expectedSquares = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100};
+		 List<Integer> numbers = list.stream()
+	                .map(n -> n * n)
+	                .collect(Collectors.toList());
+		 assertArrayEquals(expectedSquares, numbers.toArray());
+		 
+		 //Even number squares
+		 Integer[] expectedEvnSquares = {4,16,36,64,100};
+		 List<Integer> evenNumbers = list.stream()
+                 .filter(n -> n % 2 == 0)
+                 .map(n -> n * n)
+                 .collect(Collectors.toList());
+		 
+		 assertArrayEquals(expectedEvnSquares, evenNumbers.toArray());
+	}
+	
+	
+	@Test
+	public void treesetSorting() {
+		SortedSet<Employee> employeeSet = new TreeSet<>();
+		employeeSet.add(new Employee(2, "Ashwini", "Ghorude"));
+		employeeSet.add(new Employee(1, "Supriya", "Ghorude"));
+		employeeSet.add(new Employee(3, "Mahadev", "Ghorude"));
+		assertNotNull(employeeSet);
+		//create another employeeSet
+		SortedSet<Employee> employeeSample = new TreeSet<>();
+		employeeSample.add(new Employee(3, "Mahadev", "Ghorude"));
+		employeeSample.add(new Employee(1, "Supriya", "Ghorude"));
+		employeeSample.add(new Employee(2, "Ashwini", "Ghorude"));
+		assertNotNull(employeeSample);
+		assertEquals(employeeSet, employeeSample);
+	}
+	
+	@Test(expected = FileNotFoundException.class)
+	public void tryWithResources() throws FileNotFoundException, IOException {
+		  try(FileInputStream input = new FileInputStream("G:\\ash.txt")) {
+			  assertNotNull(input);  
+			  int data = input.read();
+			  assertNotNull(data);  
+		        while(data != -1){
+		            System.out.print((char) data);
+		            data = input.read();
+		        }
+		    }
+	}
+	
 	
 	@Test
 	public void withoutHashcodeAndEquals() {
@@ -66,7 +130,10 @@ public class JunitApplicationTests {
 			return false;
 	}
 
-
+	@Test
+	public void testStream() {
+		
+	}
   
   }
  
